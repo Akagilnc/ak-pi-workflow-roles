1fccfef1b00d6bcac59c0d8856c4a6530968b277
Akagilnc <akagilnc@gmail.com>
2026-08-28 08:09:15 +0900

    ak-roles: remove unowned dist duplicates

--- dist/host-contracts.js
+++
@@ -1,5 +0,0 @@
-import { Type } from "typebox";
-/** Local replacement for Pi AI's convenience constructor. */
-export function stringEnum(values, options = {}) {
-    return Type.Union(values.map((value) => Type.Literal(value)), options);
-}
